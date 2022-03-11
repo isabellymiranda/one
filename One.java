da429557f4c1c6edca8b0ebe391dd9c68a28b3e1
Isabelly Miranda <contatoisabellymiranda@gmail.com>
2022-03-11 01:23:57 +0000

    envio

---
+++ One.java
@@ -0,0 +1,10 @@
+public class One {
+    public static void main(String[] args) { 
+System.out.println("Valores de 150 a 300:");
+for(int i = 150; i <= 300; i++){  
+System.out.println(i);  
+
+} 
+}
+}
+
